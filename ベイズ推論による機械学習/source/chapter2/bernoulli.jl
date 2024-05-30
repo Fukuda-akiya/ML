bc59e6910f461ef3008d0768e0e5b185920d69b3
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2024-05-30 11:53:45 +0900

    Add files via upload

--- ベイズ推論による機械学習/source/chapter2/bernoulli.jl
+++ ベイズ推論による機械学習/source/chapter2/bernoulli.jl
@@ -2,6 +2,7 @@ using Random
 using Plots
 using Distributions
 using StatsBase
+using Printf
 
 # 乱数を固定値に設定
 Random.seed!(123)
@@ -23,16 +24,43 @@ counts = countmap(samples)
 #savefig("bernoulli_samples0.9.png")
 
 # Entropyの計算
-N = 1024
+N = 10
 delta = 1/N
 H = zeros(N)
 mu = zeros(N)
+mu_hat = zeros(N)
+
 
 for i in 1:N
-	mu[i] = delta * i
+	mu[i] = delta * (i-1)
+	mu_hat[i] = delta * (i-1)
 	H[i] = ( - mu[i] * log(mu[i]) ) - ( ( 1 - mu[i] ) * log( 1 - mu[i] ) )
 end
 
 # muとHの関係性を描画
-plot(mu, H, xlims=(0, 1.0), ylims=(0, 0.7), xlabel="mu", ylabel="Entropy", legend=false, linewidth=1)
-savefig("bernoulli_entropy.png")
+#plot(mu, H, xlims=(0, 1.0), ylims=(0, 0.7), xlabel="mu", ylabel="Entropy", legend=false, linewidth=1)
+#savefig("bernoulli_entropy.png")
+
+# KLダイバージェンスの計算
+function KL_cal(mu,mu_hat)
+	KL = zeros(N, N)
+	for i in 1:N
+		for j in 1:N
+			KL[i, j] = ( mu_hat[i] * log(mu_hat[i]) ) + ( (1 - mu_hat[i]) * log(1 - mu_hat[i]) ) - mu_hat[i] * log(mu[j]) - (1 - mu_hat[i]) * log(1 - mu[j])
+		end
+	end
+	return KL
+end
+
+
+result = KL_cal(mu,mu_hat)
+
+fp = open("KL_div_bern.dat","w")
+
+@printf(fp,"mu_hat\tmu\tKL\n")
+for i in 1:N
+	for j in 1:N
+		@printf(fp, "%.06e\t%.06e\t%.06e\n",mu_hat[i], mu[j], result[i,j])
+	end 
+end 
+close(fp)
