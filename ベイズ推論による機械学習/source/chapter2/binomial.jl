877da1cc13f71e4909e8cda820f57494cf3fedf7
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2024-05-30 17:10:59 +0900

    Update binomial.jl

--- ベイズ推論による機械学習/source/chapter2/binomial.jl
+++ ベイズ推論による機械学習/source/chapter2/binomial.jl
@@ -2,13 +2,13 @@ using Random
 using Distributions
 using Plots
 
-M_trial = 10
-mu = 0.2
+M_trial = 10  # 試行回数
+mu = 0.2  # パラメータ
 
 binomial_dist = Binomial(M_trial, mu)
 
-x = 0:M_trial
-y = pdf(binomial_dist, x)
+x = 0:M_trial  # 正解数
+y = pdf(binomial_dist, x)  # 確率質量関数の計算
 
 bar(x, y, ylims=(0,0.35), xticks=0:1:20,xlabel="Number of successes", ylabel="Probability", title="Binomial Distribution (n=10, p=0.2)", legend=false)
 savefig("binomial_M10_mu02.png")
